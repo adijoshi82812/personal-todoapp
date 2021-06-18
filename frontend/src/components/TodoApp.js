5d5f28920814cdffd7ea7e5217fd2a0a9a239544
= <=>
2021-06-19 02:26:57 +0530

    Added display details handler
    This handler is a modal box which contains task information of a particular task

--- frontend/src/components/TodoApp.js
+++ frontend/src/components/TodoApp.js
@@ -6,8 +6,16 @@ class TodoApp extends Component{
     constructor(){
         super();
         this.state = {
-            Data: []
+            Data: [],
+            show: false,
+            modalValue: {
+                title: "",
+                description: "",
+                completed: false
+            }
         };
+
+        this.handleModal = this.handleModal.bind(this);
     }
 
     refreshList = () => {
@@ -39,6 +47,49 @@ class TodoApp extends Component{
         .catch(err => console.log(err));
     };
 
+    Modal = ({show, handleModal, children}) => {
+        const showHideClassName = show ? "w3-modal w3-show" : "w3-modal";
+        return(
+            <div
+                className={showHideClassName}
+            >
+                <div
+                    className="w3-modal-content w3-container w3-round w3-animate-top"
+                >
+                    <button
+                        className="w3-button w3-round w3-display-topright"
+                        onClick={handleModal}
+                    >
+                        X
+                    </button>
+                    {children}
+                </div>
+            </div>
+        );
+    };
+
+    handleModal = (data) => {
+        if(!this.state.show){
+            this.setState({
+                show: true,
+                modalValue: {
+                    title: data.title,
+                    description: data.description,
+                    completed: data.completed
+                }
+            });
+        }else{
+            this.setState({
+                show: false,
+                modalValue: {
+                    title: "",
+                    description: "",
+                    completed: false
+                }
+            });
+        }
+    };
+
     render(){
         const datacomponent = this.state.Data.map(data => {
             return(
@@ -62,6 +113,16 @@ class TodoApp extends Component{
                         >
                             {data.title}
                         </label>
+                    </td>
+                    <td>
+                        <button
+                            type="button"
+                            onClick={() => this.handleModal(data)}
+                            className="w3-button w3-green w3-round"
+                            style={{ width: "100%" }}
+                        >
+                            Details
+                        </button>
                     </td>
                 </tr>
             );
@@ -98,6 +159,31 @@ class TodoApp extends Component{
                         {datacomponent}
                     </tbody>
                 </table>
+                <this.Modal
+                    show={this.state.show}
+                    handleModal={this.handleModal}
+                >
+                    <h3
+                        className="w3-center"
+                    >
+                        Details
+                    </h3>
+                    <input
+                        type="text"
+                        value={this.state.modalValue.title}
+                        readOnly
+                        className="w3-input w3-border w3-margin-bottom w3-round"
+                    />
+                    <textarea
+                        value={this.state.modalValue.description}
+                        readOnly
+                        className="w3-input w3-border w3-margin-bottom w3-padding-24 w3-round"
+                        style={{ resize: "none" }}
+                    />
+                    <p>
+                        Completed : {this.state.modalValue.completed ? "Yes" : "No"}
+                    </p>
+                </this.Modal>
             </div>
         );
     }
