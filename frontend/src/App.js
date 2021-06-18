24d467244e353cc5f2679526b56b05c4728ce655
= <=>
2021-06-18 22:26:54 +0530

    Created the main nav bar
    Created two different nav bar which conditionally renders while the user is logged in or not

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
+import Nav from './components/Nav';
+
 class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      logged_in: localStorage.getItem('token') ? true : false,
+      display_form: localStorage.getItem('token') ? "" : "login"
+    };
+  }
+
+  handleDisplayForm = (form) => {
+    this.setState({ display_form: form });
+  };
+
   render(){
     return(
       <div>
-        Todo App with authentication
+        <Nav
+          logged_in={this.state.logged_in}
+          handleDisplayForm={this.handleDisplayForm}
+        />
       </div>
     );
   }
---
+++ frontend/src/components/Nav.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+class Nav extends Component{
+    render(){
+        const logged_out_nav = (
+            <nav
+                className="w3-bar w3-blue w3-margin-bottom"
+            >
+                <button
+                    type="button"
+                    onClick={() => {this.props.handleDisplayForm('login')}}
+                    className="w3-button w3-bar-item w3-hover-white"
+                >
+                    Login
+                </button>
+                <button
+                    type="button"
+                    onClick={() => {this.props.handleDisplayForm('signup')}}
+                    className="w3-button w3-bar-item w3-hover-white"
+                >
+                    Sign Up
+                </button>
+            </nav>
+        );
+
+        const logged_in_nav = (
+            <nav
+                className="w3-bar w3-blue w-margin-bottom"
+            >
+                <button
+                    type="button"
+                    className="w3-button w3-bar-item w3-hover-white w3-right"
+                >
+                    Logout
+                </button>
+            </nav>
+        );
+
+        return this.props.logged_in ? logged_in_nav : logged_out_nav;
+    }
+}
+
+export default Nav;
